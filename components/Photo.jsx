77805b004fe7bfc5247a0b249e4bb9601bb400cf
Gopinath <gvarad2001@gmail.com>
2025-01-07 23:27:10 +0000

    Homepage completed and created relevant components

---
+++ components/Photo.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Photo = () => {
+  return (
+    <div className="w-full h-full relative flex justify-center items-center">
+      {/* Container for photo and circle */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+        className="relative w-[300px] h-[300px] xl:w-[506px] xl:h-[506px]"
+      >
+        {/* Circle animation */}
+        <motion.svg
+          className="absolute top-0 left-0 w-full h-full"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+
+        {/* Round photo */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="absolute top-0 left-0 w-full h-full rounded-full overflow-hidden mix-blend-lighten"
+        >
+          <Image
+            src="/assets/photo.jpeg"
+            priority
+            quality={100}
+            fill
+            alt=""
+            className="object-cover"
+          />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Photo;
