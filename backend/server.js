237611d95e63b0b116fcd7780b3e6bf3c158cc36
Gopinath <gvarad2001@gmail.com>
2025-01-09 19:11:12 +0000

    slight port change

--- backend/server.js
+++ backend/server.js
@@ -53,5 +53,5 @@ app.get("/", (req, res) => {
 });
 
 // Server Setup
-const PORT = process.env.PORT || 5000; // Use the PORT from .env or default to 5000
+const PORT = process.env.PORT || 5001; // Use the PORT from .env or default to 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
